a98f58cf996b226f4dd34ad40f847e3e33fce718
Kyle Andrews <kylea@page.works>
2020-04-18 15:54:58 -0400

    Debugging dynamic map

--- templates/_web-components/dynamic-map.ts
+++ templates/_web-components/dynamic-map.ts
@@ -133,6 +133,7 @@ class DynamicMap extends HTMLElement {
 	}
 
 	private init(map, drawing) {
+		console.log("init");
 		if (!this.map || this.map !== map) {
 			this.canvas = this.querySelector("canvas");
 			const bounds = this.getBoundingClientRect();
